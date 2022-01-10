050027fca957838105a3b498469354ed9c8b3c34
giuseppecastaldo <57662742+giuseppecastaldo@users.noreply.github.com>
2022-01-10 23:00:31 +0100

    Delete custom_components/cloudedge_pir directory

--- custom_components/cloudedge_pir/const.py
+++
@@ -1,8 +0,0 @@
-"""Constants for the CloudEdge PIR Integration integration."""
-
-DOMAIN = "cloudedge_pir"
-DEVICE_CLASS = "motion"
-
-CONF_WAIT_TO_RESET = "wait_to_reset"
-CONF_COUNTRY_CODE = "country_code"
-CONF_PHONE_CODE = "phone_code"
